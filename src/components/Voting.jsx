395b3fae21555e0545576359ea3cb4324507f70e
unknown <renan.lifestyle@hotmail.com>
2015-12-12 20:45:38 -0200

    Testing shallow rendering

--- src/components/Voting.jsx
+++ src/components/Voting.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import PureRenderMixin from 'react-addons-pure-render-mixin';
+
 import Winner from './Winner';
 import Vote from './Vote';
 
 export default React.createClass({
+    mixins: [PureRenderMixin],
     getPair: function () {
         return this.props.pair || [];
     },
--- src/components/Winner.jsx
+++ src/components/Winner.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import PureRenderMixin from 'react-addons-pure-render-mixin';
 
 export default React.createClass({
-   render: function() {
-       return (
-         <div className="winner">
-             Winner is {this.props.winner}!
-         </div>
-       );
-   }
+    mixins: [PureRenderMixin],
+    render: function () {
+        return (
+            <div className="winner">
+                Winner is {this.props.winner}!
+            </div>
+        );
+    }
 });
