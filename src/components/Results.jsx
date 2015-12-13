bb8af9e77c4eefe34b1b8f5d134bd52a759d7c37
unknown <renan.lifestyle@hotmail.com>
2015-12-12 23:13:25 -0200

    resulting screen

---
+++ src/components/Results.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import PureRenderMixin from 'react-addons-pure-render-mixin';
+import Tally from './Tally';
+import Winner from './Winner';
+
+export default React.createClass({
+    mixins: [PureRenderMixin],
+    render: function () {
+        return this.props.winner ?
+            <Winner ref="winner" winner={this.props.winner} /> :
+            <div className="results">
+                <Tally pair={this.props.pair} tally={this.props.tally}/>
+                <div className="management">
+                    <button ref="next"
+                            className="next"
+                            onClick={this.props.next}>
+                        Next
+                    </button>
+                </div>
+            </div>
+
+    }
+});
--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Router, {Route} from 'react-router';
+import App from './components/App';
 import Voting from './components/Voting';
-
-const pair = ['Trainspotting', '28 Days Later'];
+import Results from './components/Results';
 
 require('./style.css');
 
+const routes = (
+    <Route component={App}>
+        <Route path="/results" component={Results}/>
+        <Route path="/" component={Voting}/>
+    </Route>
+);
+
+
 ReactDOM.render(
-    <Voting pair={pair} winner="Trainspotting" />,
+    <Router>{routes}</Router>,
     document.getElementById('app')
 );
